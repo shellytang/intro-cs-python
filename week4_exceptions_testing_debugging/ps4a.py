7dfc85b999e82c282d4a5b2f972a799b9b91e55c
Shelly Tang <shellytang@gmail.com>
2018-01-30 22:59:16 -0800

    initial work on putting game together

--- week4_exceptions_testing_debugging/ps4a.py
+++ week4_exceptions_testing_debugging/ps4a.py
@@ -217,37 +217,50 @@ def playHand(hand, wordList, n):
       n: integer (HAND_SIZE; i.e., hand size required for additional points)
       
     """
-    # BEGIN PSEUDOCODE <-- Remove this comment when you code this function; do your coding within the pseudocode (leaving those comments in-place!)
     # Keep track of the total score
-
+    overallScore = 0
+    handLen = calculateHandlen(hand)
     # As long as there are still letters left in the hand:
-
+    while handLen > 0:
     # Display the hand
-
+        print('Current Hand: ', end='') # prints displayHand on same line
+        displayHand(hand)
     # Ask user for input
-
+        word = input('Enter word, or a "." to indicate that you are finished: ')
     # If the input is a single period:
-
+        if word == '.':
+            return 'Goodbye! Total score: ' + str(overallScore) + '.'
     # End the game (break out of the loop)
-
+        score = getWordScore(word, n)
     # Otherwise (the input is not a single period):
-
     # If the word is not valid:
-
+        if not isValidWord(word, hand, wordList):
+            print('Invalid word, please try again.')
+            print()
+            playHand(hand, wordList, n)
     # Reject invalid word (print a message followed by a blank line)
-
     # Otherwise (the word is valid):
-
-    # Tell the user how many points the word earned, and the updated total score, in one line followed by a blank line
-
+        if isValidWord:
+            overallScore = overallScore + score
+            print('" ' + word + ' "' + ' earned ' + str(score)  + ' points. Total:  ' + str(overallScore) + '  points')
+            print()
     # Update the hand
-
+            hand = updateHand(hand, word)
+            handLen = calculateHandlen(hand)
+    # Tell the user how many points the word earned, and the updated total score, in one line followed by a blank line
+    
     # Game is over (user entered a '.' or ran out of letters), so tell user the total score
+    print('Run out of letters. Total score:  ' + str(overallScore) + '  points.')
 
 
 #
 # Problem #5: Playing a game
 #
+wordList = loadWords()
+# print(playHand({'h':1, 'i':1, 'c':1, 'z':1, 'm':2, 'a':1}, wordList, 7))
+# playHand({'w': 1, 's': 1, 't': 2, 'a': 1, 'o': 1, 'f': 1}, wordList, 7)
+playHand({'n': 1, 'e': 1, 't': 1, 'a': 1, 'r': 1, 'i': 2}, wordList, 7)
+
 
 def playGame(wordList):
     """
