39b4c92a49194f1eb2189fa5dabfe58d3931680f
Shelly Tang <shellytang@gmail.com>
2018-01-31 13:44:59 -0800

    refactored to account for no new game handling

--- week4_exceptions_testing_debugging/ps4b.py
+++ week4_exceptions_testing_debugging/ps4b.py
@@ -127,9 +127,41 @@ def playGame(wordList):
 
     wordList: list (string)
     """
-    # TO DO... <-- Remove this comment when you code this function
-    # <-- Remove this when you code this function
-    print("playGame not yet implemented.")
+    gameSelect = input('Enter n to deal a new hand, r to replay the last hand, or e to end game: ')
+    hasHand = False
+    while gameSelect != 'e':
+        if gameSelect == 'n':
+            hasHand = True
+            compOrUser = input('Enter u to have yourself play, c to have the computer play: ')
+            while compOrUser != 'u' and compOrUser != 'c':
+                print('Invalid command.')
+                print()
+                compOrUser = input('Enter u to have yourself play, c to have the computer play: ') 
+            if compOrUser == 'u':
+                hand = dealHand(HAND_SIZE)
+                playHand(hand, wordList, HAND_SIZE)
+            if compOrUser == 'c':
+                hand = dealHand(HAND_SIZE)
+                compPlayHand(hand, wordList, HAND_SIZE)
+        elif gameSelect == 'r':
+            if hasHand:
+                compOrUser = input('Enter u to have yourself play, c to have the computer play: ')
+                while compOrUser != 'u' and compOrUser != 'c':
+                    print('Invalid command.')
+                    print()
+                    compOrUser = input('Enter u to have yourself play, c to have the computer play: ')
+                if compOrUser == 'u':
+                    playHand(hand, wordList, HAND_SIZE)
+                elif compOrUser == 'c':
+                    compPlayHand(hand, wordList, HAND_SIZE)
+                else:
+                    print('Invalid command.')
+                    print()
+            else:
+                print('You have not played a hand yet. Please play a new hand first!')
+        else:
+            print('Invalid command.')
+        gameSelect = input('Enter n to deal a new hand, r to replay the last hand, or e to end game: ')
 
 
 #
