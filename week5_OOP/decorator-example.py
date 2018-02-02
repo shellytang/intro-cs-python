88fc9f4920b956f0fc05cf06686ffcb371ed931c
Shelly Tang <shellytang@gmail.com>
2018-02-01 22:24:21 -0800

    added example

---
+++ week5_OOP/decorator-example.py
@@ -0,0 +1,11 @@
+def genOneMillion():
+    maxNum = 1000000
+    current = -1
+    while current < maxNum:
+        current += 1
+        yield current
+
+test = genOneMillion()
+print(test.__next__())
+print(test.__next__())
+print(test.__next__())
