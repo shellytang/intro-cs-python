3aedd64b1fd8a000acf3658502df2eeb66f68d6b
Shelly Tang <shellytang@gmail.com>
2018-02-13 17:19:30 -0800

    updated notes

--- week6_algo_complexity/bisection-search.py
+++ week6_algo_complexity/bisection-search.py
@@ -28,7 +28,7 @@ def bisect_search1(L, e):
 def bisect_search2(L, e) :
 
     def bisect_search_helper(L, e, low, high):
-        if high == low: # if point is at the same place (only one element), just do the check
+        if high == low: # if point is at the same place (only one element or end of the list)
             return L[low] == e
         mid = (low + high)//2
         if L[mid] == e:
@@ -46,4 +46,4 @@ def bisect_search2(L, e) :
     else: 
         return bisect_search_helper(L, e, 0, len(L) - 1)
 
-print(bisect_search2([1, 2, 4, 5, 6, 7], 4))
+print(bisect_search2([1, 2, 4, 5, 6, 7], 9))
